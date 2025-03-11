416b648d44b67d37dc70e8eb601890203a29f571
Pranjalkumari <kumaripranjal694@gmail.com>
2025-03-11 21:38:59 +0530

    datatypes

---
+++ basics/03_comparision.js
@@ -0,0 +1,12 @@
+console.log("2"==2)// conversion happen
+//=== strictly check
+console.log("2"===2) // also check data type
+
+//avoid this type of comparision
+console.log(null>0)
+console.log(null==0)
+console.log(null>= 0)
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>= 0)
+console.log(undefined === null)
